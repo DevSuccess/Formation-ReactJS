ebcf9731d535dd170d04e62a51faca51fbac627d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-16 21:11:55 +0300

    Cycle de vie

--- app.js
+++ app.js
@@ -18,7 +18,42 @@ class Welcome extends React.Component {
   }
 }
 
-ReactDOM.render(
-  <Welcome name="Muriel">, Comment CV ? </Welcome>,
-  document.getElementById("app")
-);
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { date: new Date() };
+    this.timer = null;
+  }
+
+  componentDidMount() {
+    this.timer = window.setInterval(this.tick.bind(this), 1000);
+  }
+
+  componentwillUnmount() {
+    window.clearInterval(this.timer);
+  }
+
+  tick() {
+    this.setState({ date: new Date() });
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.date.toLocaleDateString()}{" "}
+        {this.state.date.toLocaleTimeString()}
+      </div>
+    );
+  }
+}
+
+function Home() {
+  return (
+    <div>
+      <Welcome name="Muriel">On est : </Welcome>
+      <Clock />
+    </div>
+  );
+}
+
+ReactDOM.render(<Home />, document.getElementById("app"));
