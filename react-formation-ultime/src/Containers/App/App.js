23b5d41326c3143aa9221b1b4d3098dda2253c7d
ITDevSuccess <raharisontsidiany@gmail.com>
2023-05-18 15:34:47 +0300

    Modifier le state

--- react-formation-ultime/src/Containers/App/App.js
+++ react-formation-ultime/src/Containers/App/App.js
@@ -11,15 +11,29 @@ class App extends React.Component {
       {
         nom: "Eva Dupont",
         moyenne: 15,
-        citation: "\"Allez toujours plus loins !\"",
+        citation: '"Allez toujours plus loins !"',
       },
       { nom: "Timothée Galo", moyenne: 5, citation: null },
     ],
   };
+
+  buttonClickedHandler = () => {
+    this.setState({
+      eleves: [
+        {
+          nom: "Elon Musk",
+        },
+      ],
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Bienvenue</h1>
+
+        <button onClick={this.buttonClickedHandler}>Mon Bouton</button>
+
         {this.state.eleves.map((value, k) => (
           <Eleve key={k} nom={value.nom} moyenne={value.moyenne}>
             {value.citation}
